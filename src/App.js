5e3804ffad411a32f75328aabb1b3c4fe768a928
Aemal Sayer <a@reactgeeks.com>
2022-07-10 21:13:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const passGen = (len ) => {
+  const length = (len)?(len):(10);
+  const string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+  const numeric = '0123456789';
+  const punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  let password = "";
+  let character = "";
+  
+  while( password.length<length ) {
+      const entity1 = Math.ceil(string.length * Math.random()*Math.random());
+      const entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+      const entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+      let hold = string.charAt( entity1 );
+      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+      character += hold;
+      character += numeric.charAt( entity2 );
+      character += punctuation.charAt( entity3 );
+      password = character;
+  }
+  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+  return password.substr(0,len);
+}
+
 function App() {
+  const [password, setPassword] = useState()
+  
+  useEffect(() => {
+    setPassword(passGen(32));
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <header className="App-header">
+     {password}
       </header>
     </div>
   );
